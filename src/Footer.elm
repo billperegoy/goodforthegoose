3923af63c6f0cbe3f3d9a8181a4f0ed54325d931
Bill Peregoy <bill@gnar.dog>
2018-12-23 17:22:28 -0500

    footer styling

--- src/Footer.elm
+++ src/Footer.elm
@@ -1,6 +1,6 @@
 module Footer exposing (view)
 
-import Element exposing (Element, paddingXY, fill, width, column, px, height, el, paragraph, text, none, newTabLink, centerX)
+import Element exposing (Element, image, paddingXY, paddingEach, fill, width, column, px, height, el, paragraph, text, none, newTabLink, centerX)
 import Element.Font as Font
 import Element.Background as Background
 import Styles
@@ -14,22 +14,39 @@ view =
         [ footerSpacer
         , column
             [ width fill
-            , Background.color Styles.colors.greenGrey
+            , Background.color Styles.colors.mediumGrey
             , Font.color Styles.colors.white
-            , paddingXY 0 20
+            , paddingEach { left = 0, right = 0, top = 20, bottom = 40 }
+            , Styles.fontFamilies.hind
             , Font.center
             ]
-            [ contactLink
-            , paragraph [] [ text "Vegan owned and operated." ]
-            , paragraph [] [ text "We are not affiliated with Farm Sanctuary, we just think they’re cool." ]
+            [ logo
+            , contactLink
+            , paragraph [ paddingEach { top = 20, bottom = 0, left = 0, right = 0 }, Font.size 14 ] [ text "Vegan owned and operated." ]
+            , paragraph [ Font.size 14 ] [ text "We are not affiliated with Farm Sanctuary, we just think they’re cool." ]
             ]
         ]
 
 
+logo : Element msg
+logo =
+    image
+        [ height (px 50)
+        , centerX
+        ]
+        { src = "/goose-footer-logo-transparent.png"
+        , description = "Good for the Goose Logo"
+        }
+
+
 contactLink =
     newTabLink
-        [ Font.color Styles.colors.white, centerX ]
-        { url = "info@goodforthegoose.com"
+        [ Font.color Styles.colors.white
+        , Font.size 16
+        , centerX
+        , paddingEach { top = 0, bottom = 20, left = 0, right = 0 }
+        ]
+        { url = "mailto:info@goodforthegoose.com"
         , label = text "contact"
         }
 
@@ -39,5 +56,6 @@ footerSpacer =
     el
         [ width fill
         , height (px 6)
+        , centerX
         ]
         none
